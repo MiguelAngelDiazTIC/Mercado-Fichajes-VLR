f7b679f2a6e99e5c982c123a9c72f4e7a3712041
Miguel Ángel Díaz <miguelangeldiaztic@gmail.com>
2026-06-12 02:34:30 +0200

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import teamsRouter from './routes/teams'
 const app = express()
 const PORT = process.env.PORT || 8080
 
-app.use(cors({ origin: '*' }))
+app.use(cors({ origin: 'https://mercado-fichajes-vlr.vercel.app' }))
 app.use(express.json())
 app.use('/api/teams', teamsRouter)
 
